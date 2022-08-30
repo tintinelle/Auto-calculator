2568987196ae34a2625541800d941be7f6694893
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-08-31 00:34:07 +0500

    failed to fix

--- assets/script/index.js
+++ assets/script/index.js
@@ -12,25 +12,25 @@ const driveTypes = document.querySelectorAll('input[name="drive"]');
 const carAge = document.querySelectorAll('option[name="age"]');
 // console.log(carAge);
 
-const mileage = document.querySelector('input[name="mileage"]');
+const mileage = document.querySelectorAll('input[name="mileage"]');
+let mileageValue = mileage.value;
+// console.log(mileageValue);
 // console.log(mileage);
+const highMileage = 150000;
+const mediumMileage = 100000;
+const lowMileage = 50000;
+
 
 const additionalElements = document.querySelectorAll('input[name="additional"]');
 // console.log(additionalElements);
 
-let totalPrice = 0;
 const button = document.getElementById('calculate');
 
-
 const countPrice = () => {
-    // getCarModelValue();
-    // getEngineValue();
-    // getDriveTypelValue();
-    // getMileageValue();
-    // getCarAgeValue();
-    // getAdditionalElementsValue();
-    const mileageValue = mileage.value;
+    let totalPrice = 0;
+    mileageValue = +mileageValue;
     console.log(mileageValue);
+    console.log(typeof mileageValue);
 
     for (let i = 0; i < carModels.length; i++) {
         if (carModels[i].selected == true) {
@@ -56,30 +56,38 @@ const countPrice = () => {
         }
     }
 
-    if (+mileageValue >= 150000) {
-        totalPrice *= 0.7;
-    }
-    else if (+mileageValue >= 100000 && +mileageValue < 150000) {
-        totalPrice *= 0.8;
-    }
-    else if (+mileageValue >= 50000 && +mileageValue < 100000) {
-        totalPrice *= 1;
-    }
-    else if (+mileageValue >= 30000 && +mileageValue < 50000) {
-        totalPrice *= 1.1;
-    }
-    else {
-        totalPrice *= 1.2;
-    }
-
     for (let i = 0; i < additionalElements.length; i++) {
         if (additionalElements[i].checked == true) {
             totalPrice += +additionalElements[i].value;
         }
     }
 
+    if (mileageValue >= highMileage) {
+        totalPrice *= 0.7;
+        console.log(totalPrice);
+    } else if (mileageValue <= highMileage && mileageValue > mediumMileage) {
+        console.log(totalPrice);
+    } else {
+        totalPrice *= 1.2;
+        console.log(totalPrice);
+    }
+
     alert(totalPrice);
-}
+
+};
 
 
 button.addEventListener('click', countPrice);
+
+// if (+mileageValue >= 150000) {
+//     totalPrice *= 0.7;
+// } else 
+// if (+mileageValue >= 100000 && +mileageValue < 150000) {
+//     totalPrice *= 0.8;
+// } else if (+mileageValue >= 50000 && +mileageValue < 100000) {
+//     totalPrice *= 1;
+// } else if (+mileageValue >= 30000 && +mileageValue < 50000) {
+//     totalPrice *= 1.1;
+// } else {
+//     totalPrice *= 1.2;
+// }
